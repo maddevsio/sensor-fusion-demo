cda73ec8cfe3f9c9938312404a384628b55336a3
Lezh1k <lezh1k.vohrer@gmail.com>
2019-04-11 16:47:58 +0600

    Fixed madgwick filter initial values for android Xiomi mi4

--- app/src/main/java/org/hitlabnz/sensor_fusion_demo/orientationProvider/MadgwickProvider.java
+++ app/src/main/java/org/hitlabnz/sensor_fusion_demo/orientationProvider/MadgwickProvider.java
@@ -3,7 +3,6 @@ package org.hitlabnz.sensor_fusion_demo.orientationProvider;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorManager;
-import android.util.Log;
 
 import org.hitlabnz.sensor_fusion_demo.representation.MatrixF4x4;
 
@@ -15,11 +14,10 @@ public class MadgwickProvider extends OrientationProvider {
 
     private float gain;
     private float sampleFreq;
-    private float q0, q1, q2, q3; //quaternion
+    private float qW, qX, qY, qZ; //quaternion
 
     private float acc[] = new float[4];
     private float gyr[] = new float[4];
-    private float mag[] = new float[4];
 
     /**
      * Initialises a new OrientationProvider
@@ -32,121 +30,16 @@ public class MadgwickProvider extends OrientationProvider {
         super(sensorManager);
         this.gain = gain;
         this.sampleFreq = sampleFreq;
-        q0 = 1.0f;
-        q1 = q2 = q3 = 0.0f;
-        sensorList.add(sensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD));
+        qW = 1.0f;
+        qX = 0.0f;
+        qY = 0.0f;
+        qZ = 1.0f;
         sensorList.add(sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER));
         sensorList.add(sensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE));
     }
 
     private float invSqrt(float x) {
         return (float) (1.0f / Math.sqrt(x));
-    }
-
-    public void MadgwickAHRSupdate(float gx, float gy, float gz,
-                                   float ax, float ay, float az,
-                                   float mx, float my, float mz) {
-        float recipNorm;
-        float s0, s1, s2, s3;
-        float qDot1, qDot2, qDot3, qDot4;
-        float hx, hy;
-        float _2q0mx, _2q0my;
-        float _2q0mz, _2q1mx;
-        float _2bx, _2bz;
-        float _4bx, _4bz;
-        float _2q0, _2q1, _2q2, _2q3;
-        float _2q0q2, _2q2q3;
-        float q0q0, q0q1, q0q2, q0q3;
-        float q1q1, q1q2, q1q3;
-        float q2q2, q2q3, q3q3;
-
-        // Use IMU algorithm if magnetometer measurement invalid (avoids NaN in magnetometer normalisation)
-        if((mx == 0.0f) && (my == 0.0f) && (mz == 0.0f)) {
-            MadgwickAHRSupdateIMU(gx, gy, gz, ax, ay, az);
-            return;
-        }
-
-        // Rate of change of quaternion from gyroscope
-        qDot1 = 0.5f * (-q1 * gx - q2 * gy - q3 * gz);
-        qDot2 = 0.5f * (q0 * gx + q2 * gz - q3 * gy);
-        qDot3 = 0.5f * (q0 * gy - q1 * gz + q3 * gx);
-        qDot4 = 0.5f * (q0 * gz + q1 * gy - q2 * gx);
-
-        // Compute feedback only if accelerometer measurement valid (avoids NaN in accelerometer normalisation)
-        if(!((ax == 0.0f) && (ay == 0.0f) && (az == 0.0f))) {
-
-            // Normalise accelerometer measurement
-            recipNorm = invSqrt(ax * ax + ay * ay + az * az);
-            ax *= recipNorm;
-            ay *= recipNorm;
-            az *= recipNorm;
-
-            // Normalise magnetometer measurement
-            recipNorm = invSqrt(mx * mx + my * my + mz * mz);
-            mx *= recipNorm;
-            my *= recipNorm;
-            mz *= recipNorm;
-
-            // Auxiliary variables to avoid repeated arithmetic
-            _2q0mx = 2.0f * q0 * mx;
-            _2q0my = 2.0f * q0 * my;
-            _2q0mz = 2.0f * q0 * mz;
-            _2q1mx = 2.0f * q1 * mx;
-            _2q0 = 2.0f * q0;
-            _2q1 = 2.0f * q1;
-            _2q2 = 2.0f * q2;
-            _2q3 = 2.0f * q3;
-            _2q0q2 = 2.0f * q0 * q2;
-            _2q2q3 = 2.0f * q2 * q3;
-            q0q0 = q0 * q0;
-            q0q1 = q0 * q1;
-            q0q2 = q0 * q2;
-            q0q3 = q0 * q3;
-            q1q1 = q1 * q1;
-            q1q2 = q1 * q2;
-            q1q3 = q1 * q3;
-            q2q2 = q2 * q2;
-            q2q3 = q2 * q3;
-            q3q3 = q3 * q3;
-
-            // Reference direction of Earth's magnetic field
-            hx = mx * q0q0 - _2q0my * q3 + _2q0mz * q2 + mx * q1q1 + _2q1 * my * q2 + _2q1 * mz * q3 - mx * q2q2 - mx * q3q3;
-            hy = _2q0mx * q3 + my * q0q0 - _2q0mz * q1 + _2q1mx * q2 - my * q1q1 + my * q2q2 + _2q2 * mz * q3 - my * q3q3;
-            _2bx = (float) Math.sqrt(hx * hx + hy * hy);
-            _2bz = -_2q0mx * q2 + _2q0my * q1 + mz * q0q0 + _2q1mx * q3 - mz * q1q1 + _2q2 * my * q3 - mz * q2q2 + mz * q3q3;
-            _4bx = 2.0f * _2bx;
-            _4bz = 2.0f * _2bz;
-
-            // Gradient decent algorithm corrective step
-            s0 = -_2q2 * (2.0f * q1q3 - _2q0q2 - ax) + _2q1 * (2.0f * q0q1 + _2q2q3 - ay) - _2bz * q2 * (_2bx * (0.5f - q2q2 - q3q3) + _2bz * (q1q3 - q0q2) - mx) + (-_2bx * q3 + _2bz * q1) * (_2bx * (q1q2 - q0q3) + _2bz * (q0q1 + q2q3) - my) + _2bx * q2 * (_2bx * (q0q2 + q1q3) + _2bz * (0.5f - q1q1 - q2q2) - mz);
-            s1 = _2q3 * (2.0f * q1q3 - _2q0q2 - ax) + _2q0 * (2.0f * q0q1 + _2q2q3 - ay) - 4.0f * q1 * (1 - 2.0f * q1q1 - 2.0f * q2q2 - az) + _2bz * q3 * (_2bx * (0.5f - q2q2 - q3q3) + _2bz * (q1q3 - q0q2) - mx) + (_2bx * q2 + _2bz * q0) * (_2bx * (q1q2 - q0q3) + _2bz * (q0q1 + q2q3) - my) + (_2bx * q3 - _4bz * q1) * (_2bx * (q0q2 + q1q3) + _2bz * (0.5f - q1q1 - q2q2) - mz);
-            s2 = -_2q0 * (2.0f * q1q3 - _2q0q2 - ax) + _2q3 * (2.0f * q0q1 + _2q2q3 - ay) - 4.0f * q2 * (1 - 2.0f * q1q1 - 2.0f * q2q2 - az) + (-_4bx * q2 - _2bz * q0) * (_2bx * (0.5f - q2q2 - q3q3) + _2bz * (q1q3 - q0q2) - mx) + (_2bx * q1 + _2bz * q3) * (_2bx * (q1q2 - q0q3) + _2bz * (q0q1 + q2q3) - my) + (_2bx * q0 - _4bz * q2) * (_2bx * (q0q2 + q1q3) + _2bz * (0.5f - q1q1 - q2q2) - mz);
-            s3 = _2q1 * (2.0f * q1q3 - _2q0q2 - ax) + _2q2 * (2.0f * q0q1 + _2q2q3 - ay) + (-_4bx * q3 + _2bz * q1) * (_2bx * (0.5f - q2q2 - q3q3) + _2bz * (q1q3 - q0q2) - mx) + (-_2bx * q0 + _2bz * q2) * (_2bx * (q1q2 - q0q3) + _2bz * (q0q1 + q2q3) - my) + _2bx * q1 * (_2bx * (q0q2 + q1q3) + _2bz * (0.5f - q1q1 - q2q2) - mz);
-            recipNorm = invSqrt(s0 * s0 + s1 * s1 + s2 * s2 + s3 * s3); // normalise step magnitude
-            s0 *= recipNorm;
-            s1 *= recipNorm;
-            s2 *= recipNorm;
-            s3 *= recipNorm;
-
-            // Apply feedback step
-            qDot1 -= gain * s0;
-            qDot2 -= gain * s1;
-            qDot3 -= gain * s2;
-            qDot4 -= gain * s3;
-        }
-
-        // Integrate rate of change of quaternion to yield quaternion
-        q0 += qDot1 * (1.0f / sampleFreq);
-        q1 += qDot2 * (1.0f / sampleFreq);
-        q2 += qDot3 * (1.0f / sampleFreq);
-        q3 += qDot4 * (1.0f / sampleFreq);
-
-        // Normalise quaternion
-        recipNorm = invSqrt(q0 * q0 + q1 * q1 + q2 * q2 + q3 * q3);
-        q0 *= recipNorm;
-        q1 *= recipNorm;
-        q2 *= recipNorm;
-        q3 *= recipNorm;
     }
 
     public void MadgwickAHRSupdateIMU(float gx, float gy, float gz, float ax, float ay, float az) {
@@ -156,10 +49,10 @@ public class MadgwickProvider extends OrientationProvider {
         float _2q0, _2q1, _2q2, _2q3, _4q0, _4q1, _4q2 ,_8q1, _8q2, q0q0, q1q1, q2q2, q3q3;
 
         // Rate of change of quaternion from gyroscope
-        qDot1 = 0.5f * (-q1 * gx - q2 * gy - q3 * gz);
-        qDot2 = 0.5f * (q0 * gx + q2 * gz - q3 * gy);
-        qDot3 = 0.5f * (q0 * gy - q1 * gz + q3 * gx);
-        qDot4 = 0.5f * (q0 * gz + q1 * gy - q2 * gx);
+        qDot1 = 0.5f * (-qX * gx - qY * gy - qZ * gz);
+        qDot2 = 0.5f * (qW * gx + qY * gz - qZ * gy);
+        qDot3 = 0.5f * (qW * gy - qX * gz + qZ * gx);
+        qDot4 = 0.5f * (qW * gz + qX * gy - qY * gx);
 
         // Compute feedback only if accelerometer measurement valid (avoids NaN in accelerometer normalisation)
         if(!((ax == 0.0f) && (ay == 0.0f) && (az == 0.0f))) {
@@ -171,25 +64,25 @@ public class MadgwickProvider extends OrientationProvider {
             az *= recipNorm;
 
             // Auxiliary variables to avoid repeated arithmetic
-            _2q0 = 2.0f * q0;
-            _2q1 = 2.0f * q1;
-            _2q2 = 2.0f * q2;
-            _2q3 = 2.0f * q3;
-            _4q0 = 4.0f * q0;
-            _4q1 = 4.0f * q1;
-            _4q2 = 4.0f * q2;
-            _8q1 = 8.0f * q1;
-            _8q2 = 8.0f * q2;
-            q0q0 = q0 * q0;
-            q1q1 = q1 * q1;
-            q2q2 = q2 * q2;
-            q3q3 = q3 * q3;
+            _2q0 = 2.0f * qW;
+            _2q1 = 2.0f * qX;
+            _2q2 = 2.0f * qY;
+            _2q3 = 2.0f * qZ;
+            _4q0 = 4.0f * qW;
+            _4q1 = 4.0f * qX;
+            _4q2 = 4.0f * qY;
+            _8q1 = 8.0f * qX;
+            _8q2 = 8.0f * qY;
+            q0q0 = qW * qW;
+            q1q1 = qX * qX;
+            q2q2 = qY * qY;
+            q3q3 = qZ * qZ;
 
             // Gradient decent algorithm corrective step
             s0 = _4q0 * q2q2 + _2q2 * ax + _4q0 * q1q1 - _2q1 * ay;
-            s1 = _4q1 * q3q3 - _2q3 * ax + 4.0f * q0q0 * q1 - _2q0 * ay - _4q1 + _8q1 * q1q1 + _8q1 * q2q2 + _4q1 * az;
-            s2 = 4.0f * q0q0 * q2 + _2q0 * ax + _4q2 * q3q3 - _2q3 * ay - _4q2 + _8q2 * q1q1 + _8q2 * q2q2 + _4q2 * az;
-            s3 = 4.0f * q1q1 * q3 - _2q1 * ax + 4.0f * q2q2 * q3 - _2q2 * ay;
+            s1 = _4q1 * q3q3 - _2q3 * ax + 4.0f * q0q0 * qX - _2q0 * ay - _4q1 + _8q1 * q1q1 + _8q1 * q2q2 + _4q1 * az;
+            s2 = 4.0f * q0q0 * qY + _2q0 * ax + _4q2 * q3q3 - _2q3 * ay - _4q2 + _8q2 * q1q1 + _8q2 * q2q2 + _4q2 * az;
+            s3 = 4.0f * q1q1 * qZ - _2q1 * ax + 4.0f * q2q2 * qZ - _2q2 * ay;
             recipNorm = invSqrt(s0 * s0 + s1 * s1 + s2 * s2 + s3 * s3); // normalise step magnitude
             s0 *= recipNorm;
             s1 *= recipNorm;
@@ -204,24 +97,22 @@ public class MadgwickProvider extends OrientationProvider {
         }
 
         // Integrate rate of change of quaternion to yield quaternion
-        q0 += qDot1 * (1.0f / sampleFreq);
-        q1 += qDot2 * (1.0f / sampleFreq);
-        q2 += qDot3 * (1.0f / sampleFreq);
-        q3 += qDot4 * (1.0f / sampleFreq);
+        qW += qDot1 * (1.0f / sampleFreq);
+        qX += qDot2 * (1.0f / sampleFreq);
+        qY += qDot3 * (1.0f / sampleFreq);
+        qZ += qDot4 * (1.0f / sampleFreq);
 
         // Normalise quaternion
-        recipNorm = invSqrt(q0 * q0 + q1 * q1 + q2 * q2 + q3 * q3);
-        q0 *= recipNorm;
-        q1 *= recipNorm;
-        q2 *= recipNorm;
-        q3 *= recipNorm;
+        recipNorm = invSqrt(qW * qW + qX * qX + qY * qY + qZ * qZ);
+        qW *= recipNorm;
+        qX *= recipNorm;
+        qY *= recipNorm;
+        qZ *= recipNorm;
     }
 
     boolean init0 = false;
     boolean init1 = false;
-    boolean init2 = false;
 
-    float[] testAcc = new float[4];
     @Override
     public void onSensorChanged(SensorEvent event) {
         switch(event.sensor.getType()) {
@@ -229,28 +120,18 @@ public class MadgwickProvider extends OrientationProvider {
                 System.arraycopy(event.values, 0, acc, 0, 3);
                 init0 = true;
                 break;
-            case Sensor.TYPE_MAGNETIC_FIELD:
-                System.arraycopy(event.values, 0, mag, 0, 3);
-                init1 = true;
-                break;
             case Sensor.TYPE_GYROSCOPE:
                 System.arraycopy(event.values, 0, gyr, 0, 3);
-                init2 = true;
+                init1 = true;
                 break;
             default:
                 return;
         }
 
-        if (init0 && init1 && init2) {
-            MadgwickAHRSupdate(gyr[0], gyr[1], gyr[2], acc[0], acc[1], acc[2], mag[0], mag[1], mag[2]);
-//            Log.i("Madgwick", String.format("%f %f %f %f\n", q0, q1, q2, q3));
-            currentOrientationQuaternion.setXYZW(q1, q2, q3, -q0); //-q for cube rotation inversion
-            MatrixF4x4 m4x4 = currentOrientationQuaternion.getMatrix4x4();
-            float[] m4x4f = m4x4.getMatrix();
-            android.opengl.Matrix.multiplyMV(testAcc, 0, m4x4f,
-                    0, acc, 0);
-            Log.i("Madgwick", String.format("%f %f %f %f", testAcc[0], testAcc[1], testAcc[2], testAcc[3]));
-            init0 = init1 = init2 = false;
+        if (init0 && init1) {
+            MadgwickAHRSupdateIMU(gyr[0], gyr[1], gyr[2], acc[0], acc[1], acc[2]);
+            currentOrientationQuaternion.setXYZW(qX, qY, qZ, -qW); //-q for cube rotation inversion
+            init0 = init1 = false;
         }
     }
 }
